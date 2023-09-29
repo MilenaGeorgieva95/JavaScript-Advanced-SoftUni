a8fd495ee8c279b500166adb7270b0e9010f138b
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2023-09-29 16:37:15 +0100

    Delete 07.DOM Introduction - Lab directory

--- 07.DOM Introduction - Lab/01.editElement.js
+++
@@ -1,5 +0,0 @@
-function editElement(el, match, replacer) {
-  let text = el.innerText;
-  let newText = text.split(match).join(replacer);
-  el.innerText = newText;
-}
--- 07.DOM Introduction - Lab/07.extractParenthesis.js
+++
@@ -1,9 +0,0 @@
-function extract(id) {
-  let text = document.querySelector("#" + id).innerText;
-
-  let pattern = /\([\w ]+\)/g;
-  let matches = text.match(pattern);
-  if (matches) {
-    return matches.join("; ");
-  }
-}
